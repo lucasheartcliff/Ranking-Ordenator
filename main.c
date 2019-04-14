af0764dde509db9111e30bbba09cc7c172225082
Lucas Morais <lucascdemorais@outlook.com>
2019-04-14 13:53:25 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -24,44 +24,15 @@ void clean(void){
     system("cls");
 }
 
-void set_value(char p_word[] , int *p_acc, int *p_lenght,mapa *mp){
-    /**/
-    int acc = *p_acc;
-    int lenght = *p_lenght;
-
-
-    printf("\nNa funcao : %s\n",p_word);
-    if(mp->list == NULL){
-        printf("Memoria Insuficiente \n\n Erro: #0002");
-        exit(1);
-    }else{
-        //printf("Foi ate aqui");
-
-        mp->list[acc]->word = (char *) malloc( lenght *  sizeof(char) );
-
-       // printf("\n%s\n",mp->list[*p_acc]->word);
-
-
-        if(mp->list[acc]->word == NULL){
-            printf("Memoria Insuficiente \n\n Erro: #0003");
-            exit(1);
-
-        }else{
-
-            //printf("Foi ate aqui #2");
-
-                strcpy(mp->list[acc]->word ,p_word);
-
-            //strcpy(mp->list[acc]->word, p_word);
-               // printf("\nPalavra : %c\n",mp->list[*p_acc]->word[i]);
-
-            printf("\nPalavra Struct : %s | Indice : %i | Palavra : %s\n\n",mp->list[acc]->word, *p_acc, p_word);
-            free(mp->list[acc]->word);
-        }
-
-    }
+void pause(void){
+    /*Pausa no sistema*/
+    system("pause");
 }
 
+/*Funções*/
+void insert_word(FILE *file,mapa *mp,int *sum, int *p); //
+
+void scan_file(mapa *mp);
 
 int read_all(char now[], char before[]){
     /*Verifica se o arquivo chamado ja foi lido por completo*/
@@ -76,42 +47,12 @@ int read_all(char now[], char before[]){
     }
 }
 
-void scan_file(FILE *file, int *sum,int *p){
-    /**/
+void scan_file(mapa *mp){
     char now[buffer]; // Vai guardar a palavra atual.
     char pre[buffer]; // Vai guardar a palavra anterior.
-    int i=0;
+    int *p = (int *) malloc(sizeof(int)); // Somente para não ficar vazia
+    int i=0,sum=0;
 
-    while( fscanf(file,"%s",now) && !(read_all(now,pre)) ){ //
-            (*sum)++;
-    }
-
-    p = malloc( (*sum) * sizeof(int));
-
-    while( fscanf(file,"%s",now) && !(read_all(now,pre)) ){ //
-            p[i] = strlen(now);
-            i++;
-    }
-}
-
-void insert_word(FILE *file,mapa *mp,int *sum,int *p){
-    int i;
-
-    mp->list = (item **) malloc( (*sum) * sizeof(item*));
-
-    for( i=0;i<*sum;i++){
-        mp->list[i] = (item*) malloc(sizeof(item));
-        mp->list[i]->word = malloc(p[i] * sizeof(char));
-
-        fscanf(file,"%s",mp->list[i]->word);
-        mp->list[i]->count = p[i];
-    }
-}
-
-void read_text(mapa *mp){
-
-    int sum=0;
-    int *p;
     FILE *file;
 
     file = fopen("teste.txt","r");
@@ -124,28 +65,81 @@ void read_text(mapa *mp){
         printf("Erro abrir o arquivo!!\n\n");
         printf("Erro : #0001\n\n");
     }else{
-       scan_file(file,&sum,p);
-       insert_word(file,mp,&sum,p);
+        while( fscanf(file,"%s",now) && !(read_all(now,pre)) ){ //
+            sum++;
+        }
+
+        fclose(file);
+
+        p = (int*) (realloc(p,sum * sizeof(int)));
+
+        if(p == NULL){
+            clean();
+            printf("Memória Insuficente.\n\n Erro : #0002");
+            exit(1);
+        }else{
+
+            strcpy(now,"");
+            strcpy(pre,"");
+
+            file = fopen("teste.txt","r");
+
+            while( fscanf(file,"%s",now) && !(read_all(now,pre)) ){ //
+               p[i] = (strlen(now));
+               i++;
+            }
+            fclose(file);
+            insert_word(file,mp,&sum,p);
+        }
+    }
+}
+
+void insert_word(FILE *file,mapa *mp,int *sum, int *p){
+    int i;
+    file = fopen("teste.txt","r");
+
+    mp->list = (item **) malloc((*sum) * sizeof(item*));
+
+    if(mp->list == NULL){
+       clean();
+       printf("Memoria Insuficiente.\n\nErro : #0003");
+       exit(1);
+    }else{
+        for(i=0;i<(*sum);i++){
+            mp->list[i] = (item *) malloc(sizeof(item));
+            if(mp->list[i] == NULL){
+                clean();
+                printf("Memoria Insuficiente.\n\nErro : #0004");
+                exit(1);
+            }else{
+                mp->list[i]->word = (char *) malloc((p[i]) * sizeof(char));
+
+                if(mp->list[i]->word == NULL){
+                    clean();
+                    printf("Memoria Insuficiente.\n\nErro : #0005");
+                    exit(1);
+                }else{
+                    fscanf(file,"%s",mp->list[i]->word);
+                    mp->list[i]->count = strlen(mp->list[i]->word);
+                    printf("Palavra : %s | Indice : %i | Tamanho : %i | Ponteiro Tamanho : %i\n",mp->list[i]->word,i,mp->list[i]->count,p[i]);
+                }
+            }
+        }
     }
     fclose(file);
+    free(p);
 }
 
 int main(void){
 
     mapa *mp = (mapa *) malloc(sizeof(mapa));
-    int i;
-    mp->list = (item **) malloc( 4 * sizeof(item*));
 
-   /*for(i=0;i<4;i++){
-        mp->list[i] = (item *) malloc(sizeof(item));
+    if(mp == NULL){
+        clean();
+        printf("Falta de Memoria para inicializar.\n\nErro : #0001");
+        exit(1);
+    }else{
+        scan_file(mp);
     }
-
-    mp->list[0]->word= (char *) malloc(5*sizeof(char));
-    mp->list[1]->word= (char *) malloc(5*sizeof(char));
-    mp->list[2]->word= (char *) malloc(5*sizeof(char));
-    mp->list[3]->word= (char *) malloc(5*sizeof(char));
-*/
-    read_text(mp);
-
     return 0;
 }
